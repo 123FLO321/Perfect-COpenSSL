994572364c560f002acecad688170b96aaab13fe
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-08-02 12:00:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,29 +1,12 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-//
-//  Package.swift
-//  COpenSSL
-//
-//  Created by Kyle Jessup on 3/22/16.
-//	Copyright (C) 2016 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "COpenSSL",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "COpenSSL", targets: ["COpenSSL"])
     ],
